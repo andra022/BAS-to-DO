203e0f70c7ab7835132fc59ebbb775e20fa4da6d
Andra Belu <andra.belu@3pillarglobal.com>
2022-08-08 13:46:45 +0300

    Changed some design stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import "./App.css"
 import Form from"../components/form"
+import { Link } from 'gatsby'
 
 const HomePage = () => {
     return(
@@ -10,6 +11,17 @@ const HomePage = () => {
             <h1>BAS to DO</h1>
          </header>
          <p align="center">A simple way to create "to do" lists</p>
+         <Link
+            href="https://fonts.googleapis.com/css?family=Poppins&display=swap"
+            rel="stylesheet"
+         ></Link>
+        <Link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css"
+            integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk="
+            crossorigin="anonymous"
+        ></Link>
+        <Link rel="stylesheet" href="./style.css" ></Link>
          <Form />
       </div>
     )
